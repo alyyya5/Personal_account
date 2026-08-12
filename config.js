c102fb7a4251e64957a21e3668cfec30c14cabb7
alyyya5 <alina.memetova07@gmail.com>
2026-08-12 20:18:55 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 // ============================================================
 const GITHUB_OWNER = 'alyyya5';  
 const GITHUB_REPO  = 'Personal_account'; 
-const GITHUB_TOKEN = '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';  
+const GITHUB_TOKEN = 'GITHUB_TOKEN_PLACEHOLDER';
 
 // ============================================================
 //  🧠  ВСПОМОГАТЕЛЬНЫЕ ФУНКЦИИ (общие для всех страниц)
