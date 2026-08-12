a9befa0512ca9027ab3e3ddc3790cd956f88255f
alyyya5 <alina.memetova07@gmail.com>
2026-08-12 18:55:04 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+// ============================================================
+//  🔑  НАСТРОЙКИ GITHUB (общие для всех страниц)
+// ============================================================
+const GITHUB_OWNER = 'alyyya5';  
+const GITHUB_REPO  = 'Personal_account'; 
+const GITHUB_TOKEN = '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';  
